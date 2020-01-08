aa9e28e528f5dbceccb8d8d1d2d6d8f3a4468058
tommy colella <tcolella@bsd220.org>
2020-01-08 10:21:27 -0600

    Initial Commit

---
+++ git er dun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git er dun
+//
+//  Created by Tommy Colella on 1/8/20.
+//  Copyright © 2020 Tommy Colella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ git er dunTests/git_er_dunTests.swift
@@ -0,0 +1,34 @@
+//
+//  git_er_dunTests.swift
+//  git er dunTests
+//
+//  Created by Tommy Colella on 1/8/20.
+//  Copyright © 2020 Tommy Colella. All rights reserved.
+//
+
+import XCTest
+@testable import git_er_dun
+
+class git_er_dunTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
